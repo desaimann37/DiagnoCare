04ff562eef00fdf5aa45780a1c2cf38b3f448d2c
desaimann37 <desaimann37@gmail.com>
2024-01-25 09:00:40 +0530

    commit...

--- dignocare/src/components/navbar/DropDownprofile.js
+++ dignocare/src/components/navbar/DropDownprofile.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function DropDownprofile() {
+const DropDownprofile = () => {
 
   return (
     <div className='flex flex-col dropDownProfile'> 
         <ul className='flex flex-col gap-4'>
             <li>Profile</li>
             <li>Settings</li>
-            <li>Logout</li>
+            {/* <button onClick={}>Logout</button> */}
         </ul>
     </div>  
     )
