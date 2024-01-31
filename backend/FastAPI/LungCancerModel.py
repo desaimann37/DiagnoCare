76fb072366611308ffcdb081831a2da120395910
tanisha0907la <117159897+tanisha0907la@users.noreply.github.com>
2024-02-01 00:13:16 +0530

    Endpoint created for lung cancer prediction

---
+++ backend/FastAPI/LungCancerModel.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel #provides user friendly errors when data is invalid.
+
+class LungCancerModel(BaseModel):
+    Age : int
+    Gender : int
+    AirPollution  : int
+    Smoking : int
+    Fatigue : int
+    WeightLoss : int
+    ShortnessofBreath  : int
+    Wheezing : int
+    SwallowingDifficulty : int
+    ClubbingofFingerNails  : int
+    FrequentCold : int
+    DryCough : int
