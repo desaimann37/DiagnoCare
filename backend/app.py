f168c0940c136efff49de07e0c59ee46c671cd4d
desaimann37 <desaimann37@gmail.com>
2024-02-16 20:47:21 +0530

    modified

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-from auth import auth_bp, auth_collection
-from extension import db , jwt , MONGO_URI
-from users import user_bp
-# from flask_mongoengine import MongoEngine
-# from models import TokenBlocklist
-
 from pymongo import MongoClient
 import os
 import openai
 from openai import OpenAI
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 # from config import OPENAI_API_KEY
-
-import tensorflow as tf
-from PIL import Image
-import numpy as np
-import io 
+# import tensorflow as tf
+# from PIL import Image
+# import numpy as np
+# import io 
 from auth import auth_bp
 from extension import db
 # import tensorflow as tf
@@ -28,23 +21,15 @@ from extension import db
 # from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
-"""
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'sdp_backend',
-    'host': MONGO_URI,
-}
-db_instance = MongoEngine(app)
-
-"""
-
 CORS(app)
 
-load_dotenv()
+# load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 app.secret_key = 'your_secret_key'
 
+
+"""
 # Connect to MongoDB
 client = MongoClient('mongodb+srv://dm_37:SWKIOAkzdQgoWn68@cluster0.u4wm1ik.mongodb.net/sdp_backend')
 db = client['sdp_backend']
@@ -101,77 +86,15 @@ def api_login():
     else:
         return jsonify({'message': 'User not found'}), 404
 
-
->>>>>>> f9f1ef8ba90f07f3bf4a5a64f6c96be86888308c
+"""
 # Mongodb Name : 
 print(db.name)
-
-jwt.init_app(app)
 
 
 # Register blue_print : 
 app.register_blueprint(auth_bp , url_prefix='/auth')
-app.register_blueprint(user_bp , url_prefix='/users')
-
-class CustomUser:
-    def __init__(self, user_dict):
-        self.name = user_dict.get('name')
-        self.email = user_dict.get('email')
 
 
-
-# load user : 
-@jwt.user_lookup_loader
-def user_lookup_callback(__jwt_headers , jwt_data):
-    identity = jwt_data['sub']
-    # print(type(identity))
-    user_dict = auth_collection.find_one({'name': identity})
-    if user_dict:
-        return CustomUser(user_dict)
-    else:
-        raise LookupError("User not found")
-
-
-
-# additional claims(while getting jwt parameters in postman)
-@jwt.additional_claims_loader
-def make_additional_claims(identity):
-    if identity == "abc":
-        return {"is_staff" : True}
-    return {"is_staff": False}
-
-
-
-# jwt error handlers : 
-@jwt.expired_token_loader
-def expired_token_callback(jwt_header, jwt_data):
-    return jsonify({"message" : "Token has expired", "error": "token_expired"}), 401
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return jsonify({"message" : "Signature Varification failed", "error": "invalid_token"}), 401 
-
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return jsonify({"message" : "Request does not contain valid token", "error": "authorization_header"}), 401
-
-"""
-@jwt.token_in_blocklist_loader
-def token_in_blocklist_callback(jwt_header , jwt_data):
-    jti = jwt_data['jti']
-    token = TokenBlocklist.__objects(TokenBlocklist.jti == jti)
-
-    return token is not None
-"""
-    
-
-
-
-
-
-
-
-    
 """
 model = tf.keras.models.load_model('../Models/alzheimer2.h5')
 
@@ -208,8 +131,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
--- backend/extension.py
+++ backend/extension.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 
 
 
@@ -10,7 +10,7 @@ db = mongo.get_database()
 backend = db['sdp_backend']
 auth_collection = backend['auth']
 
-jwt = JWTManager()
+# jwt = JWTManager()
 
 
 
