a2d324777ec9c7475e998cf7bc53550ef9c9e4ff
tanisha0907la <117159897+tanisha0907la@users.noreply.github.com>
2024-03-29 20:53:42 +0530

    overview completed

--- frontend/src/DoctorComponents/DoctorLayout/Overview.js
+++ frontend/src/DoctorComponents/DoctorLayout/Overview.js
@@ -1,73 +1,75 @@
-import React from "react";
+import axios from "axios";
 import StarRoundedIcon from "@mui/icons-material/StarRounded";
 import StarHalfRoundedIcon from "@mui/icons-material/StarHalfRounded";
 import "./overview.css";
+import React, { useEffect, useState } from "react";
+import LoadingPage from "../../PatientComponents/LoadingPage";
 
-// Temporary doctor object
-const doctor = {
-  id: 1,
-  name: "Dr. John Doe",
-  Bio: "I am a Cardiologist",
-  image:
-    "https://t4.ftcdn.net/jpg/02/60/04/09/240_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
-  specialty: "Cardiologist",
-  rating: 4.5,
-  price: 400,
-  availableSlots: [
-    { day: "Monday", timing: "4:30pm-7:30pm" },
-    { day: "Wednesday", timing: "5:00pm-8:00pm" },
-  ],
-  about:
-    "Dr. John Doe is a highly experienced Cardiologist with expertise in treating various heart conditions.",
-  education: [
-    { year: "2008-2010", degree: "BSc degree in Neuroscience" },
-    { year: "2010-2014", degree: "PhD in Cardiology" },
-  ],
-  experience: [
-    {
-      year: "2003-2004",
-      position: "Intern",
-      location: "New York Hospital",
-    },
-    {
-      year: "2004-2006",
-      position: "Resident",
-      location: "Boston Medical Center",
-    },
-  ],
-  reviews: [
-    {
-      userName: "Alice",
-      userPhoto:
-        "https://t4.ftcdn.net/jpg/02/60/04/09/240_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
-      date: "June 27, 2023",
-      text: "Great doctor!",
-    },
-    {
-      userName: "Bob",
-      userPhoto:
-        "https://t4.ftcdn.net/jpg/02/60/04/09/240_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
-      date: "July 5, 2023",
-      text: "Very knowledgeable.",
-    },
-  ],
-};
 
 const Overview = () => {
+  const [doctors, setDoctors] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const getDoctorDetails = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          console.error("Token is missing.");
+          return;
+        }
+        const response = await axios.get(
+          "http://127.0.0.1:5000/doctor/profile",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response.data);
+        setDoctors(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching doctor details:", error);
+      }
+    };
+
+    getDoctorDetails();
+  }, []);
+  
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.toLocaleString('default', { month: 'long' });
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+  };
+
+   if (loading) {
+    return (
+      <>
+        <center>
+          <LoadingPage/>
+        </center>
+      </>
+    );
+  }
+
   return (
     <div className="overview-container">
       <div className="overview-horizontal-container">
         <div className="overview-left">
           <img
-            src={doctor.image}
+            src={`data:image/jpeg;base64,${doctors.photo.$binary.base64}`}
             alt="Doctor"
             className="overview-doctor-image"
           />
         </div>
         <div className="overview-right">
-          <h2>{doctor.name}</h2>
+         <h2>{doctors.name}</h2>
           <p>
-            <span className="specialty">{doctor.specialty}</span>
+            <span className="specialty">{doctors.specialization}</span>
           </p>
           <div className="ratingDiv">
             <span className="doctor-rating">
@@ -81,13 +83,13 @@ const Overview = () => {
                 }}
               >
                 {" "}
-                {doctor.rating} (2)
+                {doctors.rating} (reviews)
               </span>
             </span>
           </div>
           <br />
           <div className="overview-bio">
-            <p>{doctor.Bio}</p>
+            <p>{doctors.bio}</p>
           </div>
         </div>
       </div>
@@ -95,40 +97,87 @@ const Overview = () => {
         <div className="about-section">
           <br />
           <h2>
-            About <span>{doctor.name}</span>
+            About <span>{doctors.name}</span>
           </h2>
           <br />
-          <p>{doctor.about}</p>
+          <p>{doctors.about}</p>
 
           <br />
           <h2>Education</h2>
           <br />
           <ul>
-            {doctor.education.map((edu, index) => (
-              <li key={index}>
-                <span>{edu.year}</span>
-                <br />
-                {edu.degree}
-                <br />
-                <br />
-              </li>
-            ))}
-          </ul>
+                {doctors.qualifications && doctors.qualifications.map((edu, index) => {
+                  const startDate = new Date(edu.startDate.$date);
+                  const endDate = new Date(edu.endDate.$date);
+                  const startYear = startDate.getFullYear();
+                  const endYear = endDate.getFullYear();
+
+                  return (
+                    <li key={index}>
+                      <span>
+                        {startYear} - {endYear}
+                      </span>
+                      <br />
+                      {edu.degree}
+                      <br />
+                      {edu.university}
+                      <br />
+                      <br />
+                    </li>
+                  );
+                })}
+              </ul>
 
           <div className="experience-section">
             <h3>Experience</h3>
             <div className="experience-cards">
-              {doctor.experience.map((exp, index) => (
-                <div className="experience-card" key={index}>
-                  <h5>
-                    <span>{exp.year}</span>
-                  </h5>
-                  <h5>{exp.position}</h5>
-                  <p>{exp.location}</p>
-                </div>
-              ))}
+            {doctors.experiences && doctors.experiences.map((exp, index) => {
+                    const startDate = new Date(exp.startDate.$date);
+                    const endDate = new Date(exp.endDate.$date);
+                    const startYear = startDate.getFullYear();
+                    const endYear = endDate.getFullYear();
+                    return (
+                      <div className="experience-card" key={index}>
+                        <h5>
+                          <span>
+                            {startYear} - {endYear}
+                          </span>
+                        </h5>
+                        <h5>{exp.position}</h5>
+                        <p>{exp.location}</p>
+                      </div>
+                    );
+                  })}
             </div>
           </div>
+
+          <h3>All Reviews ({doctors.reviews && doctors.reviews.length})</h3>
+          <div className="feedback-section scrollable-reviews">
+                
+                {doctors.reviews && doctors.reviews.map((review, index) => (
+                  <div className="review" key={index}>
+                    <div className="user-profile">
+                      <img
+                        src={`data:image/jpeg;base64,${review.patient_photo.$binary.base64}`}
+                        alt="User"
+                        className="user-photo"
+                      />
+                    </div>
+                    <div className="review-details">
+                      <h4 className="user-name">{review.patient_name}</h4>
+                      <p className="review-date">{formatDate(review.review_date.$date)}</p>
+                      <div className="rating">
+                        {Array.from({ length: review.rating }, (_, i) => (
+                          <StarRoundedIcon key={i} />
+                        ))}
+                      </div>
+                      <br />
+                      <p className="review-text">{review.review_content}</p>
+                    </div>
+                    <br />
+                  </div>
+                ))}
+              </div>
         </div>
       </div>
     </div>
