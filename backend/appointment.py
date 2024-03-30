93f20e22e0a3a457b46c09768d711deec5551fdb
Isha Paghdal <ishapaghdal@gmail.com>
2024-03-30 14:42:17 +0530

    Payment succsess remaining things,my booking page in patient and video calling after appointment booking

--- backend/appointment.py
+++ backend/appointment.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, request, jsonify
-from extension import appointment_collection
+from extension import appointment_collection , auth_collection 
 from flask_jwt_extended import jwt_required, current_user, get_jwt_identity
 from datetime import datetime
+from bson import ObjectId
+from doctor import get_doctor_by_doctorId
+import requests
+import base64
 
 appointment_bp = Blueprint('appointment', __name__)
 
@@ -23,18 +27,96 @@ def add_appointment():
         }
         appointment_collection.insert_one(appointment_data)
         
-        return jsonify({'message': 'Appointment added successfully'}), 200
+        appointment_id = str(appointment_data['_id'])
+
+        return jsonify({'message': 'Appointment added successfully', 'appointment_id': appointment_id}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@appointment_bp.route('/doctor', methods=['GET'])
+@jwt_required()
+def get_appointments_with_patients_by_doctor():
+    try:
+        doctor_id = current_user.id
+        doctor_data = auth_collection.find_one({'_id': ObjectId(doctor_id)})
+        
+        appointments_cursor = appointment_collection.find({'doctor_id': doctor_id})
+        appointments = []
+        
+        for appointment in appointments_cursor:
+            patient_id = appointment['patient_id']
+            
+            patient_data = auth_collection.find_one({'_id': ObjectId(patient_id)})
+            
+            if patient_data:
+                patient_photo = base64.b64encode(patient_data['photo']).decode('utf-8')
+                
+                appointment = {
+                    'patient_id': str(patient_data['_id']), 
+                    'patient_name': patient_data['name'],
+                    'patient_email': patient_data['email'],
+                    'patient_photo': patient_photo,
+                    'appointment_date': appointment['booking_date'],
+                    'price': appointment['price'],
+                    'doctor_name': doctor_data['name'],
+                    'doctor_email': doctor_data['email']
+                }
+                
+                appointments.append(appointment)
+            else:
+                error_message = f"Failed to fetch patient patient_id: {patient_id}"
+                print(error_message)
+        
+        return jsonify({'appointments': appointments}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@appointment_bp.route('/patient', methods=['GET'])
+@jwt_required()
+def get_appointments_with_doctors_by_patient():
+    try:
+        patient_id = current_user.id
+        patient_data = auth_collection.find_one({'_id': ObjectId(patient_id)})
+        
+        appointments_cursor = appointment_collection.find({'patient_id': patient_id})
+        appointments = []
+        
+        for appointment in appointments_cursor:
+            doctor_id = appointment['doctor_id']
+            
+            doctor_data = auth_collection.find_one({'_id': ObjectId(doctor_id)})
+            
+            if doctor_data:
+                doctor_photo = base64.b64encode(doctor_data['photo']).decode('utf-8')
+                appointment_id = str(appointment['_id'])
+
+                appointment = {
+                    'appointment_id': appointment_id,
+                    'doctor_id': str(doctor_data['_id']),  
+                    'doctor_name': doctor_data['name'],
+                    'doctor_email': doctor_data['email'],
+                    'doctor_photo': doctor_photo,
+                    'appointment_date': appointment['booking_date'],
+                    'price': appointment['price'],
+                    'specialization' : doctor_data['specialization'],
+                    'patient_name': patient_data['name'],
+                    'patient_email': patient_data['email']
+                }
+                
+                appointments.append(appointment)
+            else:
+                error_message = f"Failed to fetch doctor doctor_id: {doctor_id}"
+                print(error_message)
+        
+        return jsonify({'appointments': appointments}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 @appointment_bp.route('/list', methods=['GET'])
 def list_appointments():
     try:
-        # Example: Fetch all appointments from the appointment collection
         appointments = list(appointment_collection.find({}))
         
         return jsonify({'appointments': appointments}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-# You can add more routes for updating, deleting, or retrieving specific appointments as needed
---
+++ frontend/src/PatientComponents/Layout/MyBooking.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import StarHalfRoundedIcon from '@mui/icons-material/StarHalfRounded';
+import axios from 'axios'; 
+
+const MyBooking = () => {
+    const [doctorsWithAppointments, setDoctorsWithAppointments] = useState([]);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        const fetchDoctorsWithAppointments = async () => {
+            try {
+                const response = await axios.get(
+                    "http://127.0.0.1:5000/appointment/patient",
+                    config
+                );
+
+                setDoctorsWithAppointments(response.data.appointments);
+            } catch (error) {
+                console.error('Error fetching doctors with appointments:', error);
+            }
+        };
+
+        fetchDoctorsWithAppointments();
+    }, []);
+
+    const startConsultation = (appointmentId) => {
+        window.location.href = `/patient/meeting?roomID=${appointmentId}`;
+    };
+
+    return (
+        <div className="doctors-container">
+            {doctorsWithAppointments.map(doctor => (
+                <div className="doctorCard" key={doctor.doctor_id}>
+                    <Link to={`/patient/doctors/${doctor.doctor_id}`}>
+                        <img className="courseImg" src={`data:image/jpeg;base64,${doctor.doctor_photo}`} alt="Doctor Image"/>
+                        <h3>{doctor.doctor_name}</h3>
+                        <div className="bestsellerBadge">{doctor.specialization}</div>
+                        <div className="ratingDiv">
+                            <span className="rating">{doctor.rating}
+                                <StarRoundedIcon />
+                                <StarRoundedIcon />
+                                <StarRoundedIcon />
+                                <StarRoundedIcon />
+                                <StarHalfRoundedIcon />
+                            </span>
+                        </div>
+                        <div className="priceAndBadge">
+                            <h4 className="price">₹{doctor.price}</h4>
+                        </div>
+                        <div className="appointmentDetails">
+                            <p>Appointment booked on: {new Date(doctor.appointment_date).toLocaleString()}</p>
+                            <p>Patient Name: {doctor.patient_name}</p>
+                            <p>Patient Email: {doctor.patient_email}</p>
+                        </div>
+                    </Link>
+                    <button className="book-appointment-button" onClick={() => startConsultation(doctor.appointment_id)}>Start Consultation</button>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default MyBooking;
